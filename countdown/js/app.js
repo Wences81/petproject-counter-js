99b4299dcc27bbd53482e38c4c05a401b94f5129
Wences81 <zhylinpochta@gmail.com>
2023-11-09 19:22:49 +0100

    09/11/2023

--- countdown/js/app.js
+++ countdown/js/app.js
@@ -7,4 +7,22 @@ function getCountdownTime() {
     const now = new Date().getTime();
 
     const distance = countdownDate - now
+
+    const oneDay = 24 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    let days = Math.floor(distance / oneDay);
+    let hours = Math.floor(distance % oneDay / oneHour);
+    let minutes = Math.floor(distance % oneHour / oneMinute);
+    let seconds = Math.floor(distance % oneMinute / 1000);
+
+    const values = [days, hours, minutes, seconds];
+    console.log(values);
+
+    items.forEach(function (item, index) {
+        item.textContent = values[index];
+    });
 }
+
+let countdown = setInterval(getCountdownTime, 1000);
