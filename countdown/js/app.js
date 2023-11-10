d8d8c568838233e30ab868b4419e943d41e528ac
Wences81 <zhylinpochta@gmail.com>
2023-11-10 15:11:15 +0100

    10/11/20023

--- countdown/js/app.js
+++ countdown/js/app.js
@@ -1,14 +1,15 @@
-const items = document.querySelectorAll('.countdown-items >  h4');
+const items = document.querySelectorAll('.countdown-item >  h4');
+const countdownElement = document.querySelector('.countdown');
 
 
-let countdownDate = new Date(2022, 11, 18, 10, 0, 0).getTime();
+let countdownDate = new Date(2025, 10, 10, 15, 8, 0).getTime();
 
 function getCountdownTime() {
     const now = new Date().getTime();
 
-    const distance = countdownDate - now
+    const distance = countdownDate - now;
 
-    const oneDay = 24 * 60 * 1000;
+    const oneDay = 24 * 60 * 60 * 1000;
     const oneHour = 60 * 60 * 1000;
     const oneMinute = 60 * 1000;
 
@@ -18,11 +19,18 @@ function getCountdownTime() {
     let seconds = Math.floor(distance % oneMinute / 1000);
 
     const values = [days, hours, minutes, seconds];
-    console.log(values);
+    
 
     items.forEach(function (item, index) {
         item.textContent = values[index];
     });
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        countdownElement.innerHTML = "<h4 class='expired' >time is over</h4>"
+    }
 }
 
 let countdown = setInterval(getCountdownTime, 1000);
+
+getCountdownTime();
